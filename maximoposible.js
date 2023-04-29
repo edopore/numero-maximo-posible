a29d3be64c7352a50c3ec3f32d197a881b28b552
Eduardo José Maya Rodriguez <36246464+edopore@users.noreply.github.com>
2023-04-28 21:46:09 -0500

    Add files via upload

--- maximoposible.js
+++ maximoposible.js
@@ -2,12 +2,15 @@
  * Punto 1 Prueba técnica realizada para Quind
  * Ejercicio realizado por Eduardo Jose Maya Rodriguez
  * Lenguaje empleado JavaScript con nodeJs
+ * 
+ * 1. Escribe una función que, dada una lista de enteros no negativos,
+ * ordene los numeros de tal forma que se obtenga el numero mayor posible
+ * 
+ * Se realiza el uso de la funcion Bubble Sort que permite organizar números de manera
+ * descendente por medio de método burbuja, luego se realiza la concatenación 
+ * volviendo los números enteros en String para obtener el numero entero no negativo
+ * mayor posible.
  */
-
-const prompt = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
 /**
  * Función de metodo burbuja para ordenar de mayor a menor los números
@@ -25,15 +28,7 @@ function BubbleSort(arr){
     return arr
 
 }
-/**
- * 1. Escribe una función que, dada una lista de enteros no negativos,
- * ordene los numeros de tal forma que se obtenga el numero mayor posible
- * 
- * Se realiza el uso de la funcion Bubble Sort que permite organizar números de manera
- * descendente por medio de método burbuja, luego se realiza la concatenación 
- * volviendo los números enteros en String para obtener el numero entero no negativo
- * mayor posible.
- */
+
 function getMaxNumFromArray(numberArray){
     BubbleSort(numberArray);
     let number = "";
